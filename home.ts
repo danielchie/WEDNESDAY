83339a592bed9c01a3bba2e40927a63638957f11
danielchie <chiemeried321@gmail.com>
2022-08-09 18:14:28 +0100

    first time

---
+++ home.ts
@@ -0,0 +1,7 @@
+let fisrtname: string = 'daniel';
+let age: number = 30;
+let lastname: string = 'daniel';
+console.warn('last name variable is in camel case',lastname);
+if (typeof(age) !=='number'){
+    console.log('Age is not a number');
+}
